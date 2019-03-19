ac83c03d52bfa6ad7f83228d4917949921a5c9a5
Sandro Martis <smartis@esri.com>
2019-03-19 10:00:47 +0100

    Adds initialGeometry to drawing tool

--- example/main.jsx
+++ example/main.jsx
@@ -16,12 +16,23 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import esriLoader from 'esri-loader';
 
 import { SceneView, Scene, Layer, UI, Ground, SliceTool, DistanceMeasurementTool, DrawingTool } from 'react-sceneview'; // eslint-disable-line
 
-esriLoader.loadCss('https://js.arcgis.com/4.7/esri/css/main.css');
-
+const initialGeometry = {
+  type: 'polygon',
+  rings: [
+    [-8188825.165368124, 5702959.769073752],
+    [-8188716.615804167, 5703128.839067342],
+    [-8188563.815668999, 5703041.904958983],
+    [-8188626.09936864, 5702945.593375879],
+    [-8188825.165368124, 5702959.769073752],
+  ],
+  spatialReference: {
+    wkid: 102100,
+    latestWkid: 3857,
+  },
+};
 
 render(
   <SceneView id="sceneview">
@@ -38,12 +49,12 @@ render(
         zoomTo
         selectable
         url="https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Building_Montreal/SceneServer"
-        onLoad={({ id }) => console.log(id)}
-      />
-      <DistanceMeasurementTool
-        onChange={e => console.log(e)}
       />
     </Scene>
+    <DrawingTool
+      onDraw={e => console.log(e)}
+      initialGeometry={initialGeometry}
+    />
   </SceneView>,
   document.getElementById('root'),
 );
