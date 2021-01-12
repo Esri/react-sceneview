d41f5ba321557dbb68b633d889885bc517ddda06
lstaehli <lstaehli@esri.com>
2021-01-12 15:25:26 +0100

    make prettier & linter work nicely together

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,12 @@ module.exports = {
     es6: true,
     jest: true,
   },
-  extends: [
-    'airbnb',
-  ],
+  extends: ['airbnb', 'prettier'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2018,
-    ecmaFeatures : {
-      jsx : true,
+    ecmaFeatures: {
+      jsx: true,
     },
   },
   rules: {
@@ -21,12 +19,12 @@ module.exports = {
     'implicit-arrow-linebreak': 0,
     'arrow-parens': 0,
     'prefer-destructuring': 0,
-    'object-curly-newline' :0,
+    'object-curly-newline': 0,
     'import/no-named-as-default': 0,
     'react/destructuring-assignment': 0,
     'react/forbid-prop-types': 0,
     'react/no-access-state-in-setstate': 0,
-    'react/jsx-fragments' :0,
+    'react/jsx-fragments': 0,
     'react/jsx-wrap-multilines': 0,
     'react/jsx-props-no-spreading': 0,
   },
