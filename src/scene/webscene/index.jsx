28ac2ad6d39bcf105d4d262bdaec89d276172f3d
lstaehli <lstaehli@esri.com>
2021-01-18 09:18:13 +0100

    fix layerSettings for ground layers

--- src/scene/webscene/index.jsx
+++ src/scene/webscene/index.jsx
@@ -60,9 +60,9 @@ class Webscene extends Component {
     if (prevProps.layerSettings !== this.props.layerSettings) {
       Object.keys(this.props.layerSettings).forEach(layerId => {
         const settings = this.props.layerSettings[layerId];
-        const layer = this.state.groupLayer.layers.items.find(
-          l => l.id === layerId,
-        );
+        const layer =
+          this.state.groupLayer.layers.items.find(l => l.id === layerId) ||
+          this.state.groundLayers.find(l => l.id === layerId);
         if (!layer) return;
 
         Object.keys(settings).forEach(
