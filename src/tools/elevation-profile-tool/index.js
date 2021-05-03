a1e2bb93555fc0f61dff8aeabe38fd65579421ba
lor11212 <81747273+lor11212@users.noreply.github.com>
2021-05-03 18:18:42 +0200

    Implemented the elevation profile tool react component using the js api 4.19 elevation profile widget (#86)
    * Implemented the elevation profile tool react component.
    
    * Added a callback function to the props, that is called when the component did mount.
    
    * Added profiles list to the props, as well as a few widget configuration options.

---
+++ src/tools/elevation-profile-tool/index.js
@@ -0,0 +1,78 @@
+/* Copyright 2021 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import esriLoader from 'esri-loader';
+import unitOptions from '../../helpers/unit-options';
+
+class ElevationProfileTool extends Component {
+  async componentDidMount() {
+    this.componentIsMounted = true;
+
+    const [ElevationProfile] = await esriLoader.loadModules([
+      'esri/widgets/ElevationProfile',
+    ]);
+    if (!this.componentIsMounted) return;
+
+    this.elevationProfileTool = new ElevationProfile({
+      view: this.props.view,
+      unit: this.props.unit,
+      profiles: this.props.profiles,
+      visibleElements: {
+        selectButton: this.props.displaySelectButton,
+        settingsButton: this.props.displaySettingsButton,
+      },
+    });
+
+    if (this.props.onLoad) this.props.onLoad();
+  }
+
+  componentWillUnmount() {
+    this.componentIsMounted = false;
+    if (this.elevationProfileTool) this.elevationProfileTool.destroy();
+  }
+
+  render() {
+    return null;
+  }
+}
+
+ElevationProfileTool.propTypes = {
+  unit: PropTypes.oneOf(unitOptions),
+  view: PropTypes.object,
+  onLoad: PropTypes.func,
+  profiles: PropTypes.array,
+  displaySelectButton: PropTypes.bool,
+  displaySettingsButton: PropTypes.bool,
+};
+
+ElevationProfileTool.defaultProps = {
+  unit: 'metric',
+  view: null,
+  onLoad: null,
+  profiles: [
+    {
+      type: 'ground',
+      color: '#61d4a4',
+      title: 'Ground Elevation',
+    },
+  ],
+  displaySelectButton: true,
+  displaySettingsButton: true,
+};
+
+export default ElevationProfileTool;
